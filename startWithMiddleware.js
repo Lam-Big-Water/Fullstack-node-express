8d3d415107623466a77c0403ea7500e7551fe7d5
Sam Lam <xlin67948gmail.com>
2024-08-13 21:26:13 +0800

    recap the part3 and refactor the code

--- startWithMiddleware.js
+++ startWithMiddleware.js
@@ -1,100 +1,106 @@
 const express = require('express');
 const app = express();
 
+// initial data
 let notes = [
   {
-    id: "1",
+    id: 1,
     content: "HTML is easy",
     important: true
   },
   {
-    id: "2",
+    id: 2,
     content: "Browser can execute only JavaScript",
     important: false
   },
   {
-    id: "3",
+    id: 3,
     content: "GET and POST are the most important methods of HTTP protocol",
     important: true
   }
 ]
 
-
 // middleware
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
+  console.log('Method: ', request.method);
+  console.log('Path: ', request.path);
+  console.log('Body: ', request.body);
+  console.log('---');
   next()
 }
 
 // use middleware
-app.use(express.json());
+app.use(express.json);
 app.use(requestLogger);
 
+// error handle
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'});
+};
 
+
+// API
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
+  response.send('<h1>Hello World!</h1>');
+});
 
 // GET ALL
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
-})
-// GET BY ID
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
-  const note = notes.find(note => note.id === id);
+  response.json(notes);
+});
 
+// GET SINGLE
+app.get('api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
   if (note) {
     response.json(note);
   } else {
+    console.log('X');
     response.status(404).end();
   }
-
 })
 
-// generateId => POST API
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
-
-  return String(maxId + 1)
-}
-
 // POST
-app.post('/api/notes', (request, response) => {
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
+  return maxId + 1;
+};
 
+app.post('/api/notes', (request, response) => {
   const body = request.body;
 
   if (!body.content) {
     return response.status(400).json({
       error: 'content missing'
-    })
-  }
+    });
+  };
 
   const note = {
     content: body.content,
-    important: Boolean(body.important) || false,
+    important: body.important || false,
     id: generateId(),
   }
 
-  notes = notes.concat(note)
-  console.log(note);
-  response.json(note);
-})
+  notes = notes.concat(note);
 
+  response.json(notes);
+})
 
 // DELETE
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
+  const id = Number(request.params.id);
   notes = notes.filter((note) => note.id !== id);
 
   response.status(204).end();
 })
 
+app.use(unknownEndpoint);
+
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on part ${PORT}`);
+});
+
+
+
